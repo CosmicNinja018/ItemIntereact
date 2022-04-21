0431b79838fa627b6b68bfe2d61e085cb6d1d68e
CosmicNinja018 <104138369+CosmicNinja018@users.noreply.github.com>
2022-04-21 15:57:25 +0530

    First Release

---
+++ src/CosmicNinja018/ItemInteract/Main.php
@@ -0,0 +1,78 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CosmicNinja018\ItemInteract;
+
+use pocketmine\item\Item;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\Server;
+use pocketmine\player\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\event\inventory\InventoryTransactionEvent;
+use pocketmine\event\player\PlayerDropItemEvent;
+use pocketmine\event\player\PlayerRespawnEvent;
+use function explode;
+
+class Main extends PluginBase implements Listener {
+
+    public function onEnable() : void{
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->saveDefaultConfig();
+    }
+
+    public function onJoin(PlayerJoinEvent $event) {
+        $player = $event->getPlayer();
+        $itemArray = explode(':', $this->getConfig()->get("ItemID"));
+        if(!isset($itemArray[0]) || !isset($itemArray[1]) || !isset($itemArray[2])) {
+                    $this->getLogger()->error("[ItemInteract] Config Error Make Sure To Use ID:META:COUNT!");
+        return;
+                }
+        $player = $event->getPlayer();
+        $item = Item::get((int)$itemArray[0], (int)$itemArray[1], (int)$itemArray[2]); //FORMAT ID:META:COUNT
+        $item->setCustomName("§r".$this->getConfig()->get("DisplayName"));
+        $item->getNamedTag()->setInt("ItemInteractPlugin", 1);
+        $player->getInventory()->setItem($this->getConfig()->get("HotBarSlot"), $item, true);
+    }
+
+    public function onDrop(PlayerDropItemEvent $event) {
+        $item = $event->getItem();
+        if($item->getNamedTag()->hasTag("ItemInteractPlugin"))
+            $event->setCancelled(true);
+    }
+
+    public function onClick(PlayerInteractEvent $event) {
+        $player = $event->getPlayer();
+        $item = $player->getInventory()->getItemInHand();
+        if($item->getNamedTag()->hasTag("ItemInteractPlugin"))
+            $this->getServer()->getCommandMap()->dispatch($player, $this->getConfig()->get("command"));
+    }
+
+    public function onTransaction(InventoryTransactionEvent $event) {
+        $transaction = $event->getTransaction();
+        foreach ($transaction->getActions() as $action) {
+           if($action->getSourceItem()->getNamedTag()->hasTag("ItemInteractPlugin"))
+              $event->setCancelled();
+       }
+   }  
+    
+    public function onRespawn(PlayerRespawnEvent $event) {
+      if($this->getConfig()->get("item-on-respawn") == true){
+      $player = $event->getPlayer();
+        $itemArray = explode(':', $this->getConfig()->get("ItemID"));
+        if(!isset($itemArray[0]) || !isset($itemArray[1]) || !isset($itemArray[2])) {
+                    $this->getLogger()->error("[ItemInteract] Config Error Make Sure To Use ID:META:COUNT!");
+        return;
+                }
+        $player = $event->getPlayer();
+        $item = Item::get((int)$itemArray[0], (int)$itemArray[1], (int)$itemArray[2]); //FORMAT ID:META:COUNT
+        $item->setCustomName("§r".$this->getConfig()->get("DisplayName"));
+        $item->getNamedTag()->setInt("ItemInteractPlugin", 1);
+        $player->getInventory()->setItem($this->getConfig()->get("HotBarSlot"), $item, true);
+    }
+}
+}
